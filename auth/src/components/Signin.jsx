3e7b9053f37b5860eefda0689182e4eb03797c15
Riyad Ahmed <riyadahmed2809@gmail.com>
2024-07-28 07:29:07 +0600

    logout

--- auth/src/components/Signin.jsx
+++ auth/src/components/Signin.jsx
@@ -1,39 +1,101 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import firebaseConfig from '../firebaseConfig';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
 
 function Signin() {
+
+  const auth = getAuth();
+  const navigate = useNavigate();
+
+  
+
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [err, setErr] = useState({allError: '' , email: '', password: '', passwordLength: ''});
+
+
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    let errors = {};
+
+    if (!email && !password) {
+      errors.allError = 'All fields are required';
+    }
+
+    if (!email) {
+      errors.email = 'Enter your email';
+    }
+
+    if (!password) {
+      errors.password = 'Enter your password';
+    } else if (password.length < 6) {
+      errors.passwordLength = 'Password must be at least 6 characters long';
+    }
+
+    if (Object.keys(errors).length > 0) {
+      setErr(errors);
+    } else {
+      signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        setErr({ allError: '', name: '', email: '', password: '', passwordLength: '' });
+        navigate("/");
+        
+      })
+      .catch((error) => {
+        console.log(error.code);
+        if (error.code == "auth/invalid-credential" ){
+          setErr({ password: 'The password is wrong'});
+        }
+      });
+    }
+  };
+
+
   return (
     <>
-    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-  <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-    <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
-  </div>
-
-  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form class="space-y-6" action="#" method="POST">
-      <div>
-        <label htmlFor="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-        <div class="mt-2">
-          <input id="email" name="email" type="email" autoComplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign In</h2>
         </div>
-      </div>
-
-      <div>
-        <div class="flex items-center justify-between">
-          <label htmlFor="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-          <div class="text-sm">
-            <a href="/" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
-          </div>
-        </div>
-            <div class="mt-2">
-                <input id="password" name="password" type="password" autoComplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+              <div className="mt-2">
+                <input onChange={(e)=> setEmail(e.target.value)} id="email" name="email" type="email" autoComplete="email"  className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              </div>
+              {err.email && <p className="text-red-600 !mt-0">{err.email}</p>}
             </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+              </div>
+                  <div className="mt-2">
+                      <input onChange={(e)=> setPassword(e.target.value)} id="password" name="password" type="password" autoComplete="current-password"  className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                  </div>
+              </div>
+              {err.password && <p className="text-red-600 !mt-0">{err.password}</p>}
+              {err.passwordLength && <p className="text-red-600 !mt-0">{err.passwordLength}</p>}
+              {err.allError && <p className="text-red-600 !mt-0">{err.allError}</p>}
+            <div>
+              <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 p-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign UP</button>
+            </div>
+            <div>
+              <Link to="/SignUp">Already have don't account? Sign Up</Link>
+            </div>
+          </form>
         </div>
-      <div>
-        <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
       </div>
-    </form>
-  </div>
-</div>
     </>
   )
 }
